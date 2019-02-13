b2dd7b7592f82f5da6420666c4c4dfd39d770ba3
Jack Moore <moorew1997@gmail.com>
2019-02-13 18:46:51 -0500

    FrameworkCore Added

--- api/Startup.cs
+++ api/Startup.cs
@@ -8,8 +8,10 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Fisher.Bookstore.Models;
 
 namespace Fisher.Bookstore.Api
 {
@@ -25,6 +27,7 @@ namespace Fisher.Bookstore.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BookstoreContext>(options => options.UseInMemoryDatabase("Books"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
